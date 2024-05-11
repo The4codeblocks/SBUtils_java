b2bd3a82bf4bfa871b0af3bb20d0d6ab300536cc
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2024-05-11 12:55:48 +0100

    merge classes

--- src/SBUtils/content/types/blocks/logic/DrawProcessor.java
+++ src/SBUtils/content/types/blocks/logic/DrawProcessor.java
@@ -132,88 +132,6 @@ public class DrawProcessor extends LogicBlock {
         }
     }
 
-    public class DrawProcessorBuild extends Building{
-        public FrameBuffer buffer;
-        public float color = Color.whiteFloatBits;
-        public float stroke = 1f;
-        public LongQueue commands = new LongQueue(256);
-
-        @Override
-        public void draw(){
-            super.draw();
-
-            //don't even bother processing anything when displays are off.
-            if(!Vars.renderer.drawDisplays) return;
-
-            Draw.draw(Draw.z(), () -> {
-                if(buffer == null){
-                    buffer = new FrameBuffer(displaySize, displaySize);
-                    //clear the buffer - some OSs leave garbage in it
-                    buffer.begin(Pal.darkerMetal);
-                    buffer.end();
-                }
-            });
-
-            //don't bother processing commands if displays are off
-            if(!commands.isEmpty()){
-                Draw.draw(Draw.z(), () -> {
-                    Tmp.m1.set(Draw.proj());
-                    Draw.proj(0, 0, displaySize, displaySize);
-                    buffer.begin();
-                    Draw.color(color);
-                    Lines.stroke(stroke);
-
-                    while(!commands.isEmpty()){
-                        long c = commands.removeFirst();
-                        byte type = DisplayCmd.type(c);
-                        int x = unpackSign(DisplayCmd.x(c)), y = unpackSign(DisplayCmd.y(c)),
-                        p1 = unpackSign(DisplayCmd.p1(c)), p2 = unpackSign(DisplayCmd.p2(c)), p3 = unpackSign(DisplayCmd.p3(c)), p4 = unpackSign(DisplayCmd.p4(c));
-
-                        switch(type){
-                            case commandClear -> Core.graphics.clear(x / 255f, y / 255f, p1 / 255f, 1f);
-                            case commandLine -> Lines.line(x, y, p1, p2);
-                            case commandRect -> Fill.crect(x, y, p1, p2);
-                            case commandLineRect -> Lines.rect(x, y, p1, p2);
-                            case commandPoly -> Fill.poly(x, y, Math.min(p1, maxSides), p2, p3);
-                            case commandLinePoly -> Lines.poly(x, y, Math.min(p1, maxSides), p2, p3);
-                            case commandTriangle -> Fill.tri(x, y, p1, p2, p3, p4);
-                            case commandColor -> Draw.color(this.color = Color.toFloatBits(x, y, p1, p2));
-                            case commandStroke -> Lines.stroke(this.stroke = x);
-                            case commandImage -> {
-                                var icon = Fonts.logicIcon(p1);
-                                Draw.rect(Fonts.logicIcon(p1), x, y, p2, p2 / icon.ratio(), p3);
-                            }
-                            case commandCharacter -> {
-                                //TODO
-                            }
-                        }
-                    }
-
-                    buffer.end();
-                    Draw.proj(Tmp.m1);
-                    Draw.reset();
-                });
-            }
-
-            Draw.blend(Blending.disabled);
-            Draw.draw(Draw.z(), () -> {
-                if(buffer != null){
-                    Draw.rect(Draw.wrap(buffer.getTexture()), x, y, buffer.getWidth() * scaleFactor * Draw.scl, -buffer.getHeight() * scaleFactor * Draw.scl);
-                }
-            });
-            Draw.blend();
-        }
-
-        @Override
-        public void remove(){
-            super.remove();
-            if(buffer != null){
-                buffer.dispose();
-                buffer = null;
-            }
-        }
-    }
-
     static int unpackSign(int value){
         return (value & 0b0111111111) * ((value & (0b1000000000)) != 0 ? -1 : 1);
     }
@@ -376,6 +294,85 @@ public class DrawProcessor extends LogicBlock {
     }
 
     public class DrawProcessorBuild extends Building implements Ranged{
+        public FrameBuffer buffer;
+        public float color = Color.whiteFloatBits;
+        public float stroke = 1f;
+        public LongQueue commands = new LongQueue(256);
+
+        @Override
+        public void draw(){
+            super.draw();
+
+            //don't even bother processing anything when displays are off.
+            if(!Vars.renderer.drawDisplays) return;
+
+            Draw.draw(Draw.z(), () -> {
+                if(buffer == null){
+                    buffer = new FrameBuffer(displaySize, displaySize);
+                    //clear the buffer - some OSs leave garbage in it
+                    buffer.begin(Pal.darkerMetal);
+                    buffer.end();
+                }
+            });
+
+            //don't bother processing commands if displays are off
+            if(!commands.isEmpty()){
+                Draw.draw(Draw.z(), () -> {
+                    Tmp.m1.set(Draw.proj());
+                    Draw.proj(0, 0, displaySize, displaySize);
+                    buffer.begin();
+                    Draw.color(color);
+                    Lines.stroke(stroke);
+
+                    while(!commands.isEmpty()){
+                        long c = commands.removeFirst();
+                        byte type = DisplayCmd.type(c);
+                        int x = unpackSign(DisplayCmd.x(c)), y = unpackSign(DisplayCmd.y(c)),
+                        p1 = unpackSign(DisplayCmd.p1(c)), p2 = unpackSign(DisplayCmd.p2(c)), p3 = unpackSign(DisplayCmd.p3(c)), p4 = unpackSign(DisplayCmd.p4(c));
+
+                        switch(type){
+                            case commandClear -> Core.graphics.clear(x / 255f, y / 255f, p1 / 255f, 1f);
+                            case commandLine -> Lines.line(x, y, p1, p2);
+                            case commandRect -> Fill.crect(x, y, p1, p2);
+                            case commandLineRect -> Lines.rect(x, y, p1, p2);
+                            case commandPoly -> Fill.poly(x, y, Math.min(p1, maxSides), p2, p3);
+                            case commandLinePoly -> Lines.poly(x, y, Math.min(p1, maxSides), p2, p3);
+                            case commandTriangle -> Fill.tri(x, y, p1, p2, p3, p4);
+                            case commandColor -> Draw.color(this.color = Color.toFloatBits(x, y, p1, p2));
+                            case commandStroke -> Lines.stroke(this.stroke = x);
+                            case commandImage -> {
+                                var icon = Fonts.logicIcon(p1);
+                                Draw.rect(Fonts.logicIcon(p1), x, y, p2, p2 / icon.ratio(), p3);
+                            }
+                            case commandCharacter -> {
+                                //TODO
+                            }
+                        }
+                    }
+
+                    buffer.end();
+                    Draw.proj(Tmp.m1);
+                    Draw.reset();
+                });
+            }
+
+            Draw.blend(Blending.disabled);
+            Draw.draw(Draw.z(), () -> {
+                if(buffer != null){
+                    Draw.rect(Draw.wrap(buffer.getTexture()), x, y, buffer.getWidth() * scaleFactor * Draw.scl, -buffer.getHeight() * scaleFactor * Draw.scl);
+                }
+            });
+            Draw.blend();
+        }
+
+        @Override
+        public void remove(){
+            super.remove();
+            if(buffer != null){
+                buffer.dispose();
+                buffer = null;
+            }
+        }
         /** logic "source code" as list of asm statements */
         public String code = "";
         public LExecutor executor = new LExecutor();
